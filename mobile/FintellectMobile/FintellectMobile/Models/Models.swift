93852abc9f8e688173931b920a567990b93e1429
rreusch2 <reid.reusch@gmail.com>
2025-02-01 05:03:19 -0600

    Update

--- mobile/FintellectMobile/FintellectMobile/Models/Models.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-// Re-export all models
-@_exported import struct FintellectMobile.AIInsight
-@_exported import struct FintellectMobile.AIResponse
-@_exported import struct FintellectMobile.AIMetadata 
